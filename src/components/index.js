af9e32b1c187eca13fe5c0b24e94cda0a66b0644
Tyler Ingersoll <tyler@ingersoll.dev>
2026-04-15 18:37:36 -0400

    cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as Article } from "./Article.vue";
 export { default as Header } from "./Header.vue";
 export { default as Footer } from "./Footer.vue";
 export { default as MobileNav } from "./MobileNav.vue";
-export { default as Modal } from "./Modal.vue";
 export { default as Navigation } from "./Navigation.vue";
 export { default as SocialIcons } from "./SocialIcons.vue";
 export { default as ThemeToggle } from "./ThemeToggle.vue";
